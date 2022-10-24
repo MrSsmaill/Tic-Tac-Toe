796cd917a8aa0d32dd65b36e93255f1e398ffb31
MrSsmaill <180596@mail.ru>
2022-10-24 14:07:56 +0300

    start

---
+++ main.py
@@ -0,0 +1,10 @@
+
+
+
+status = [[0, 1, 2], [1, 2, 0], [2, 1, 0]]
+
+print(status)
+
+print('_{}_|_{}_|_{}_'.format(status[0][0], status[0][1], status[0][2]))
+print('_{}_|_{}_|_{}_'.format(status[1][0], status[0][1], status[0][2]))
+print(' {} | {} | {} '.format(status[2][0], status[0][1], status[0][2]))
