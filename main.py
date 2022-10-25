921c6cb91327f393f89b52bbf5a8e2987f970d9d
MrSsmaill <180596@mail.ru>
2022-10-25 21:28:32 +0300

    Tic-Tac-Toe_1.1

--- main.py
+++ main.py
@@ -2,4 +2,6 @@ import modul
 
 status = modul.random_state()
 print(status)
-modul.print_matrix(status)
+to_status = modul.to_str(status)
+print(to_status)
+modul.print_matrix(to_status)
--- modul.py
+++ modul.py
@@ -6,27 +6,31 @@ def random_state() -> list:
     for i in range(3):
         for j in range(3):
             status[i].append(random.randint(0, 2))
-            if status[i][j] == 0 and i != 2:
-                status[i].pop(j)
-                status[i].append('___')
-            elif status[i][j] == 1 and i != 2:
-                status[i].pop(j)
-                status[i].append('_X_')
-            elif status[i][j] == 2 and i != 2:
-                status[i].pop(j)
-                status[i].append('_O_')
-            if status[i][j] == 0 and i == 2:
-                status[i].pop(j)
-                status[i].append('   ')
-            elif status[i][j] == 1 and i == 2:
-                status[i].pop(j)
-                status[i].append(' X ')
-            elif status[i][j] == 2 and i == 2:
-                status[i].pop(j)
-                status[i].append(' O ')
     return status
 
 
+def to_str(status: list) -> list:
+    status_str = []
+    i = 0
+    for elem in status:
+        for value in elem:
+            if value == 0 and i > 5:
+                status_str.append(' ')
+            elif value == 0:
+                status_str.append('_')
+            elif value == 1:
+                status_str.append('X')
+            elif value == 2:
+                status_str.append('O')
+            i += 1
+    return status_str
+
+
 def print_matrix(status: list):
-    for value in status:
-        print('{}|{}|{}'.format(value[0], value[1], value[2]))
+    i = 0
+    while i < len(status):
+        if i > 5:
+            print(' {} | {} | {} '.format(status[i], status[i + 1], status[i + 2]))
+        else:
+            print('_{}_|_{}_|_{}_'.format(status[i], status[i + 1], status[i + 2]))
+        i += 3
