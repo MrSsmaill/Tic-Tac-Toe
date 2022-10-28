30d7ab4dadabf563057d0c1c5917bd6c840e8e38
MrSsmaill <180596@mail.ru>
2022-10-28 12:09:28 +0300

    Tic-Tac-Toe_1.3

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import modul
 
+
 status = modul.random_state()
 print(status)
 status_str = modul.status_to_str(status)
 modul.print_matrix(status_str)
-
+print()
+status_str2=modul.status_to_str2(status)
+print(status_str2)
--- modul.py
+++ modul.py
@@ -9,6 +9,25 @@ def random_state() -> list:
     return status
 
 
+def row_to_str(status: list, flag: bool) -> str:
+    status_str = str()
+    mapping = {0: ' ', 1: "X", 2: "0"}
+    # rez = [mapping[k[k]] for k in range(3)]
+    for i in range(3):
+        if flag:
+            status_str += ' ' + str(mapping[status[i]]) + (' |' if i != 2 else ' ')
+        else:
+            status_str += '_' + (str(mapping[status[i]]) if status[i] != 0 else '_') + ('_|' if i != 2 else '_')
+    return status_str
+
+
+def status_to_str2(status: list) -> str:
+    str1 = row_to_str(status[0], False)
+    str2 = row_to_str(status[1], False)
+    str3 = row_to_str(status[2], True)
+    return f"{str1}\n{str2}\n{str3}"
+
+
 def status_to_str(status: list) -> str:
     status_str = str()
     for i in range(3):
