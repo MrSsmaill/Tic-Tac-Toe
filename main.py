2b1ad13aba8143b5c4030cf13bd227d188d3f460
Mrssmaill <180596@mail.ru>
2022-11-07 00:14:53 +0300

    Tic-Tac-Toe_2.0

--- main.py
+++ main.py
@@ -2,9 +2,4 @@ import modul
 
 
 status = modul.random_state()
-print(status)
-status_str = modul.status_to_str(status)
-modul.print_matrix(status_str)
-print()
-status_str2=modul.status_to_str2(status)
-print(status_str2)
+modul.the_game(status)
--- modul.py
+++ modul.py
@@ -12,12 +12,9 @@ def random_state() -> list:
 def row_to_str(status: list, flag: bool) -> str:
     status_str = str()
     mapping = {0: ' ', 1: "X", 2: "0"}
-    # rez = [mapping[k[k]] for k in range(3)]
     for i in range(3):
-        if flag:
-            status_str += ' ' + str(mapping[status[i]]) + (' |' if i != 2 else ' ')
-        else:
-            status_str += '_' + (str(mapping[status[i]]) if status[i] != 0 else '_') + ('_|' if i != 2 else '_')
+        a = ' ' if flag else '_'
+        status_str += a + (str(mapping[status[i]]) if status[i] != 0 else a) + (a + '|' if i != 2 else a)
     return status_str
 
 
@@ -28,29 +25,46 @@ def status_to_str2(status: list) -> str:
     return f"{str1}\n{str2}\n{str3}"
 
 
-def status_to_str(status: list) -> str:
-    status_str = str()
-    for i in range(3):
-        for j in range(3):
-            if i != 2:
-                a = '_'
-            else:
-                a = ' '
-            if status[i][j] == 0 and i != 2:
-                status_str += a + '_' + a
-            elif status[i][j] == 1:
-                status_str += a + 'X' + a
-            elif status[i][j] == 2:
-                status_str += a + 'O' + a
-            elif status[i][j] == 0 and i == 2:
-                status_str += a + ' ' + a
-            if j == 2:
-                status_str += '\n'
-            else:
-                status_str += '|'
-
-    return status_str
+def end_game(value:str):
+    if not value:
+        print('Игра окончена')
+        exit()
 
 
-def print_matrix(status_str: str):
-    print(status_str)
+def zone_selection(status) -> str:
+    index = input('Введите зону ')
+    end_game(index)
+    while not index.isdigit() or int(index) > 33 or int(index) < 11 or int(index[1]) > 3 or \
+            status[int(index[0]) - 1][
+                int(index[1]) - 1] != 0:
+        index = input('Вы ввели не верную зону\nВведите зону ')
+        end_game(index)
+    return index
+
+
+def value_selection()->str:
+    elem = input('Введите значение ').upper()
+    end_game(elem)
+    while elem != 'X' and elem != 'O':
+        elem = input('Вы ввели не верное значение\nВведите значение ').upper()
+        end_game(elem)
+    return elem
+
+
+def the_game(status: list):
+    status_list = [0]
+    player = 0
+    while status_list.count(0) != 0:
+        print()
+        print(status_to_str2(status))
+        print()
+        status_list.clear()
+        print('Игрок {}'.format(player % 2 + 1))
+        index = zone_selection(status)
+        elem = value_selection()
+        status[int(index[0]) - 1][int(index[1]) - 1] = 1 if elem == 'X' else 2
+        for i in range(3):
+            for j in range(3):
+                status_list.append(status[i][j])
+        player += 1
+    print('Игра окончена')
