573d2eec2d41b763fc31e466c84a69927b4ecff8
Mrssmaill <180596@mail.ru>
2022-11-07 16:39:33 +0300

    Tic-Tac-Toe_2.0.0

--- modul.py
+++ modul.py
@@ -25,3 +25,46 @@ def status_to_str2(status: list) -> str:
     return f"{str1}\n{str2}\n{str3}"
 
 
+def end_game(value:str):
+    if not value:
+        print('Игра окончена')
+        exit()
+
+
+def zone_selection(status) -> str:
+    index = input('Введите зону ')
+    end_game(index)
+    while not index.isdigit() or int(index) > 33 or int(index) < 11 or int(index[1]) > 3 or \
+            status[int(index[0]) - 1][
+                int(index[1]) - 1] != 0:
+        index = input('Вы ввели не верную зону\nВведите зону ')
+        end_game(index)
+    return index
+
+
+def value_selection()->str:
+    elem = input('Введите значение ').upper()
+    end_game(elem)
+    while elem != 'X' and elem != 'O':
+        elem = input('Вы ввели не верное значение\nВведите значение ').upper()
+        end_game(elem)
+    return elem
+
+
+def the_game(status: list):
+    status_list = [0]
+    player = 0
+    while status_list.count(0) != 0:
+        print()
+        print(status_to_str2(status))
+        print()
+        status_list.clear()
+        print('Игрок {}'.format(player % 2 + 1))
+        index = zone_selection(status)
+        elem = value_selection()
+        status[int(index[0]) - 1][int(index[1]) - 1] = 1 if elem == 'X' else 2
+        for i in range(3):
+            for j in range(3):
+                status_list.append(status[i][j])
+        player += 1
+    print('Игра окончена')
